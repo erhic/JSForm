6c73d9318790b5e9f78230eddde73c843c12a869
erhic <ericngugi24@gmail.com>
2023-06-02 00:59:25 +0300

    added a button variable and event listener , onlick and the function to execute

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
+const submitButton = document.querySelector('.button');// creating a variable for button to add a listener
+submitBtton.onclick =(e)=>{
 const fname = document.getElementById('fname').value;
 const sname = document.getElementById('sname').value;
 const lname = document.getElementById('sname').value;
 const email = document.getElementById('email').value;
 const pass = document.getElementById('pass').value;
 const cpass = document.getElementById('cpass').value;
+}
