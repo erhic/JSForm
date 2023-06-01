097ca4389ccce4b40e8db58d15b56139f1fd6191
erhic <ericngugi24@gmail.com>
2023-06-02 01:06:09 +0300

    stored input values in localstorage

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ submitBtton.onclick =(e)=>{
   e.preventDefault()
 const fname = document.getElementById('fname').value;
 const sname = document.getElementById('sname').value;
-const lname = document.getElementById('sname').value;
+const lname = document.getElementById('lname').value;
 const email = document.getElementById('email').value;
 const pass = document.getElementById('pass').value;
 const cpass = document.getElementById('cpass').value;
+
+
+
+//storing values in teh browser , localStorage
+localStorage.setItem('FirstName', fname);
+localStorage.setItem('LastName', lname);
+localStorage.setItem('Email', email);
+localStorage.setItem('Password', pass);
+localStorage.setItem('CPassword',cpass);
 }
