dd7a83d648a0645bd3027093517cabf09e6a2a87
erhic <ericngugi24@gmail.com>
2023-06-02 00:53:11 +0300

    stored the value of the input in the form in variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const fname = document.getElementById('fname').value;
+const sname = document.getElementById('sname').value;
+const lname = document.getElementById('sname').value;
+const email = document.getElementById('email').value;
+const pass = document.getElementById('pass').value;
+const cpass = document.getElementById('cpass').value;
