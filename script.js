9ede7fdb46b5921745011a6c7ecea318261d0188
erhic <ericngugi24@gmail.com>
2023-06-02 02:02:15 +0300

    added  sweetalert cdn and error and success function to dispay modal based on condition of inputs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 const submitButton = document.querySelector('.button');// creating a variable for button to add a listener
-submitBtton.onclick =(e)=>{
-  e.preventDefault()
+submitButton.onclick =(e)=>{
+  e.preventDefault();
 const fname = document.getElementById('fname').value;
-const sname = document.getElementById('sname').value;
+ const sname = document.getElementById('sname').value;
 const lname = document.getElementById('lname').value;
 const email = document.getElementById('email').value;
 const pass = document.getElementById('pass').value;
@@ -11,10 +11,48 @@ const cpass = document.getElementById('cpass').value;
 
 
 
-//storing values in teh browser , localStorage
+//storing values in the browser , localStorage
 localStorage.setItem('FirstName', fname);
 localStorage.setItem('LastName', lname);
 localStorage.setItem('Email', email);
 localStorage.setItem('Password', pass);
 localStorage.setItem('CPassword',cpass);
+
+if (fname == "" && lname == "" && email == "" && pass == "" && cpass == ""){
+  Swal.fire(
+    'Oops!',
+    'Input all values!',
+    'error'
+  );
+}
+else{
+  if(pass.length>=6 && pass.length<=20){
+    if (pass !== cpass){
+      Swal.fire(
+        'Oops!',
+        'Password not matching!',
+        'error'
+      );
+    }else{
+      Swal.fire(
+        'Good Job!',
+        'Registration Successfu!',
+        'success'
+      );
+      setTimeout(()=>{
+        location.href ='login.html'
+      },5000)
+
+    }
+  }else{
+    Swal.fire(
+      'Oops!',
+      'Input a minimum of six digits password!',
+      'error'
+    )
+  }
+}
+
+
+
 }
